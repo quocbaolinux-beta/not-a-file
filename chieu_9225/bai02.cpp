af3f58cbf68327dc546294875e670704e7d4a34b
QuocBaoLinux <thaiquocbao939@gmail.com>
2025-02-09 16:44:59 +0700

    modified:   chieu_9225/bai02.cpp

--- chieu_9225/bai02.cpp
+++ chieu_9225/bai02.cpp
@@ -11,33 +11,34 @@ int reverse_number(int n) {
     }
     return res;
 }
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); 
     cout.tie(NULL);
 
-    //freopen("bai02.inp", "r", stdin);
-    //freopen("bai02.out", "w", stdout);
+    freopen("bai02.inp", "r", stdin);
+    freopen("bai02.out", "w", stdout);
 
     cin >> p >> q;
-    bool a[100001]={true, true};
-    for(int i=2; i<=316; i++)
-    {
-        if(a[i]==false)
-        {
-            for(int j=i*i; j<=100000; j+=i)
-            {
-                a[j]=true;
+    bool a[100001];
+    fill(a, a + 100001, true);
+    a[0] = a[1] = false;
+
+    for (int i = 2; i * i <= 100000; i++) {
+        if (a[i]) {
+            for (int j = i * i; j <= 100000; j += i) {
+                a[j] = false;
             }
         }
     }
-    for(int i=p; i<=q; i++)
-    {
-        int d=reverse_number(i);
-        if(a[d]==false)
-        {
+
+    for (int i = p; i <= q; i++) {
+        int d = reverse_number(i);
+        if (a[i] && a[d]) {
             cout << i << endl;
         }
     }
+
     return 0;
 }
