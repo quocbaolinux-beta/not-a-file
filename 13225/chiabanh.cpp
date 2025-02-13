3d2c49299529a283524c521632546161d619d8ac
QuocBaoLinux <thaiquocbao939@gmail.com>
2025-02-13 16:05:56 +0700

    new file:   13225/.cph/.chiabanh.cpp_ebb9597a6e15169eb31b3aa8f5f84064.prob 	modified:   13225/chiabanh.cpp

--- 13225/chiabanh.cpp
+++ 13225/chiabanh.cpp
@@ -13,6 +13,19 @@ int main()
     freopen("chiabanh.inp", "r", stdin);
     freopen("chiabanh.out", "w", stdout);
     input();
-    
+    int maxSum = 0;
+    for (int i = 0; i < 4; ++i) {
+        maxSum += a[i];
+    }
+
+    int cs = maxSum;
+    for (int i = 1; i < 8; ++i) {
+        cs = cs - a[i - 1] + a[(i + 3) % 8];
+        if (cs > maxSum) {
+            maxSum = cs;
+        }
+    }
+
+    cout << maxSum << endl;
     return 0;
 }
